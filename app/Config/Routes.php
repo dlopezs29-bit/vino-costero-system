4014865aa4c44a4e61ebdf1de83677f7fbfb218d
Diego Lopez <dlopezs29@miumg.edu.gt>
2025-10-15 19:31:27 -0600

    Ruta principal cambiada

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,7 +5,7 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+//$routes->get('/', 'Home::index');
 
 $routes->get('login', 'Auth::index');
 $routes->post('auth/intentarLogin', 'Auth::intentarLogin');
